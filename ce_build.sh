2114caa0d2285413648c83190d342d66de6b5c19
RasPython3 <87003300+RasPython3@users.noreply.github.com>
2024-01-17 18:35:05 +0900

    Remove unnecessary

--- ce_build.sh
+++ ce_build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-export HOST_ARCH=arm-none-linux-gnu && \
-export TOOL_PREFIX=${TOOL_PREFIX:-/usr/bin/arm-mingw32ce} && \
-export CC=$TOOL_PREFIX-gcc && \
-export CXX=$TOOL_PREFIX-g++ && \
-export CPP="$TOOL_PREFIX-g++ -E" && \
-export AR=$TOOL_PREFIX-ar && \
-export RANLIB=$TOOL_PREFIX-ranlib && \
-export LD=$TOOL_PREFIX-ld && \
-export READELF=$TOOL_PREFIX-readelf && \
-export WINDRES=$TOOL_PREFIX-windres && \
-export LIBS="-lcoredll6 -lcoredll -lm -laygshell -lws2 -lcommctrl" && \
-export CFLAGS="-march=armv5tej -mcpu=arm926ej-s -Wno-attributes -DWC_NO_BEST_FIT_CHARS -D_WIN32_WCE=0x0600 -D_MAX_PATH=260 -D_UNICODE -DUNICODE -DPy_HAVE_ZLIB=1 -DLACK_OF_CRYPT_API -fvisibility=hidden -fno-pic -I./zlib-src -I./Modules/_ctypes/libffi_arm_wince" && \
-export LDFLAGS="-fno-strict-aliasing" && \
-export CPPFLAGS="-fvisibility=hidden" && \
-export LIBFFI_INCLUDEDIR="Modules/_ctypes/libffi-arm-wince" && \
+export HOST_ARCH=arm-none-linux-gnu
+export TOOL_PREFIX=${TOOL_PREFIX:-/usr/bin/arm-mingw32ce}
+export CC=$TOOL_PREFIX-gcc
+export CXX=$TOOL_PREFIX-g++
+export CPP="$TOOL_PREFIX-g++ -E"
+export AR=$TOOL_PREFIX-ar
+export RANLIB=$TOOL_PREFIX-ranlib
+export LD=$TOOL_PREFIX-ld
+export READELF=$TOOL_PREFIX-readelf
+export WINDRES=$TOOL_PREFIX-windres
+export LIBS="-lcoredll6 -lcoredll -lm -laygshell -lws2 -lcommctrl"
+export CFLAGS="-march=armv5tej -mcpu=arm926ej-s -Wno-attributes -DWC_NO_BEST_FIT_CHARS -D_WIN32_WCE=0x0600 -D_MAX_PATH=260 -D_UNICODE -DUNICODE -DPy_HAVE_ZLIB=1 -DLACK_OF_CRYPT_API -fvisibility=hidden -fno-pic -I./zlib-src -I./Modules/_ctypes/libffi_arm_wince"
+export LDFLAGS="-fno-strict-aliasing"
+export CPPFLAGS="-fvisibility=hidden"
+export LIBFFI_INCLUDEDIR="Modules/_ctypes/libffi-arm-wince"
 export ZLIBDIR="./zlib-src"
 
 PY_DEBUG='no';
